3ee42a858afaf679a5c51128c3c1932d96dc54f6
4cruzeta <4cruzetam@gmail.com>
2024-08-08 11:57:20 -0300

    Inserido campo vendor(fornecedor) no cadastro de produtos

--- products/models.py
+++ products/models.py
@@ -3,19 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-CATEGORY = (
-    ('Stationary', 'Stationary'),
-    ('Electronics', 'Electronics'),
-    ('Food', 'Food'),
-    ('Limpeza', 'Limpeza')
-)
-
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
-    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    category = models.CharField(max_length=50, null=True)
     quantity = models.PositiveIntegerField(null=True)
     barcode = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=200, null=True)
+    vendor = models.CharField(max_length=50, null=True)
 
     def __str__(self) -> str:
         return self.name
