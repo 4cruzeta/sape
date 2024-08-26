ff1df8dcb0a63ae4e28faf7e9f716fe02329827b
4cruzeta <4cruzetam@gmail.com>
2024-08-26 16:33:52 -0300

    Instalado Bootstrap 5.3.3 e configurada home page

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ def register_view(request):
         form = UserCreationForm(request.POST) 
         if form.is_valid(): 
             login(request, form.save())
-            return redirect("posts:list")
+            return redirect("/")
     else:
         form = UserCreationForm()
     return render(request, "users/register.html", { "form": form })
@@ -20,7 +20,7 @@ def login_view(request):
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
             else:
-                return redirect("posts:list")
+                return redirect("/")
     else: 
         form = AuthenticationForm()
     return render(request, "users/login.html", { "form": form })
@@ -28,4 +28,4 @@ def login_view(request):
 def logout_view(request):
     if request.method == "POST": 
         logout(request) 
-        return redirect("posts:list")
+        return redirect("/")
