29bd09d525f72d64fc34a8116385f15e9747b845
4cruzeta <4cruzetam@gmail.com>
2024-08-27 13:29:13 -0300

    Adicionado app posts

--- sape/urls.py
+++ sape/urls.py
@@ -21,5 +21,6 @@ from .views import index
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
 ]
